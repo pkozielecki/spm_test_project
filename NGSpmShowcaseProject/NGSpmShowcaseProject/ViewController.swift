8b21924f97fb9b07b555227e15a0a2a35c352cbc
Pawel Kozielecki <pawel.kozielecki@gmail.com>
2020-02-11 09:08:21 +0100

    Initial commit

---
+++ NGSpmShowcaseProject/NGSpmShowcaseProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NGSpmShowcaseProject
+//
+//  Created by Paweł Kozielecki on 11/02/2020.
+//  Copyright © 2020 Netguru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var labelSpmAdoptionRate: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
